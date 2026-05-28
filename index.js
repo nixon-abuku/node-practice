2059969cca9ca21f0bdddbaa39ce5860a51eecf2
Nixon Abuku <nixonmatara91@gmail.com>
2026-05-28 11:30:37 -0400

    add /jobs route, nodemon, npm run dev script

--- index.js
+++ index.js
@@ -1,9 +1,17 @@
 const express = require('express');
 const app = express();
 
+const jobs = [
+    {"id": 1, "company": "google", "role": "SWE", "status": "APPLIED"},
+    {"id": 2, "company": "Meta", "role": "Data Analyst", "status": "REJECTED"},
+    {"id": 3, "company": "Amazon", "role": "Project Manage", "status": "INTERVIEW"}
+]
 app.get('/', function(req, res) { 
-    res.send("Hello World");
+    res.json({"message": "Hello Nixon"});
     });
+app.get('/jobs', function(req, res){
+    res.json(jobs);
+});
 
 app.listen(3000, function(){
     console.log("Server started")
