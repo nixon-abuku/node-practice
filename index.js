e6335951476a5f6f3a1022db8f98b68bafb79162
Nixon Abuku <nixonmatara91@gmail.com>
2026-06-01 13:51:22 -0400

    add POST /jobs route with req.body and 201 status

--- index.js
+++ index.js
@@ -1,18 +1,33 @@
 const express = require('express');
 const app = express();
+app.use(express.json());
 
 const jobs = [
     {"id": 1, "company": "google", "role": "SWE", "status": "APPLIED"},
     {"id": 2, "company": "Meta", "role": "Data Analyst", "status": "REJECTED"},
     {"id": 3, "company": "Amazon", "role": "Project Manage", "status": "INTERVIEW"}
 ]
+
 app.get('/', function(req, res) { 
     res.json({"message": "Hello Nixon"});
     });
 app.get('/jobs', function(req, res){
-    res.json(jobs);
+    res.status(200).json(jobs);
 });
 
+//POST 
+app.post('/jobs', function(req, res){
+    const newJob = {
+        id: jobs.length + 1,
+        company: req.body.company,
+        role: req.body.role,
+        status: req.body.status
+    }
+    jobs.push(newJob);
+    res.status(201).json(newJob);
+})
+
+
 app.listen(3000, function(){
-    console.log("Server started")
+    console.log("Server started");
 });
