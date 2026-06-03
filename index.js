35ea261bfb8296dbb0f6ac7d6544b39de9e8bb47
Nixon Abuku <nixonmatara91@gmail.com>
2026-06-03 12:40:17 -0400

    add DELETE /jobs/:id route - complete CRUD

--- index.js
+++ index.js
@@ -38,7 +38,16 @@ app.put('/jobs/:id', function(req, res){
     foundJob.status = req.body.status;
     res.status(200).json({"Message":"Job Updated"});
 });
-
+app.delete('/jobs/:id', function(req, res){
+    const jobIndex = jobs.findIndex(function(job){
+        return job.id === Number(req.params.id);
+    });
+    if (jobIndex === -1){
+        return res.status(404).json({"Message": "Job not found"});
+    }
+     jobs.splice(jobIndex, 1);
+     res.status(200).json({"Message": "Job deleted Successfully"});
+});
 
 app.listen(3000, function(){
     console.log("Server started");
